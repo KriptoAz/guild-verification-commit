11f8fa550c4e858a89e1fbbd03f44d2c6557d782
KriptoAz <139686529+KriptoAz@users.noreply.github.com>
2026-03-06 02:46:32 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T23:46:32.209Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T23:46:33.689Z
+Commit: Resolve timezone issues in date handling
