824059cf8b25b39959ed9c9474a80b9b3a9731b9
KriptoAz <139686529+KriptoAz@users.noreply.github.com>
2026-03-06 02:45:51 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T23:45:48.082Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T23:45:52.498Z
+Commit: Fix broken links in documentation
