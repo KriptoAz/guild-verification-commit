35c9a61b7d9b2d1e15c6a43b0c9728aff657029b
KriptoAz <139686529+KriptoAz@users.noreply.github.com>
2026-03-06 02:46:31 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T23:46:17.393Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T23:46:32.209Z
+Commit: Patch API endpoint error handling
