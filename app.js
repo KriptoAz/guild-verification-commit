6f3eef91a54e97d8602fc62c65f075b79804f77e
KriptoAz <139686529+KriptoAz@users.noreply.github.com>
2026-03-06 02:44:21 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T23:44:21.132Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T23:44:22.348Z
+Commit: Fix CSS styling bug on mobile devices
