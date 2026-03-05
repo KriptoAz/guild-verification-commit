34e9993a3d7076e958df207fd532b728bb3e4bc8
KriptoAz <139686529+KriptoAz@users.noreply.github.com>
2026-03-06 02:46:06 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T23:45:53.490Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T23:46:07.762Z
+Commit: Fix CSS styling bug on mobile devices
